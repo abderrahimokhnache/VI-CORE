da0ca58f0263e4af1b89dab5ed42311bf0a1a7d9
abderrahim mokhnache <abderrahimokhnache@gmail.com>
2022-01-14 16:00:11 -1100

    toke off intents && added Core test

--- Core/CEREBRUM.py
+++ Core/CEREBRUM.py
@@ -3,38 +3,33 @@ from Qalm import json_pen
 from utilities.logger import logerr
 
 class Think():
-	
+	check = lambda master_input,patterns :any([pattern in master_input for pattern in patterns]) 
+	get_patter = lambda tag :[each_string.lower() for each_string in tag['patterns']]	
 	def __init__(self,input_):
-		tag , response = Think.tokenize(input_)
+
+		tag , response = Think.features(input_)
 		self.output = random.choice(response)
 		self.tag = tag
 
 	@staticmethod
-	def tokenize(master_input=''):
-		load = json_pen("Sandbox/dep/intents.json",'r')
-		check = lambda master_input,patterns :any([pattern in master_input for pattern in patterns]) 
-		get_patter = lambda tag :[each_string.lower() for each_string in tag['patterns']]
-		
-		for tag in load['intents'] :
-			#see if the input in the intents file
-			if check(master_input,get_patter(tag)) :
-		 		return(tag['tag'] , tag['responses'])
-
-		#see if the input in any of the skills discription
+	def features(master_input=''):
 		for file in os.listdir(Sandbox.__path__[0]):
 			if ".py" in file and "__init__" not in file  :
-				#getting the model name
 				model = file.split(".")[0]
-				#importing the model
 				__import__('Sandbox.%s'%model)
-				#gettting the discription dict
 				tag =(Think.exec_and_return(('Sandbox.%s.discription()'%model)))
-				#looking for hte user input inside the dict patterns 
-				if check(master_input,get_patter(tag)) :
-						#if there's a match the userinput will be giving as an arg for the execute func
+				if Think.check(master_input,Think.get_patter(tag)) :
 						eval((f'Sandbox.{model}.execute(master_input)'))
 						return(tag['tag'] , tag['responses'])
 		return (master_input , ["Not in the system"])
+		
+	@staticmethod
+	def noaction(master_input):
+		load = json_pen("Sandbox/dep/intents.json",'r')
+		for tag in load['intents'] :
+			if Think.check(master_input,Think.get_patter(tag)) :
+		 		return(tag['tag'] , tag['responses'])
+
 	@staticmethod
 	def exec_and_return(expression):
 	    exec(f"""locals()['temp'] = {expression}""")
--- test_general.py
+++ test_general.py
@@ -1,9 +1,14 @@
-from cgitb import reset
 import unittest
 import Sandbox.search_system as searchsys
+from Core import CEREBRUM
 
 class TestFeatures(unittest.TestCase):
     def test_searchsys(self):
         exe = lambda x:searchsys.execute(x)
         self.assertEqual(exe('search') , None)
-        
+
+    def test_Cerebrum(self):
+        exe = CEREBRUM.Think('what time is it')
+        self.assertTrue(exe)       
+
+    
