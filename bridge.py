4437a192fe8f18c7f32e9ba9d4dc0a85e119c791
abderrahim mokhnache <abderrahimokhnache@gmail.com>
2022-01-14 22:34:41 -1100

    added wakeup def

--- bridge.py
+++ bridge.py
@@ -2,12 +2,13 @@ from Core.CEREBRUM import Think
 from utilities.spark import Listen ,Talk
 from utilities.memory import save_todisstree
 
-greet = Talk("Ready for you sir")
+def wakeup():
+	greet = Talk("Ready for you sir")
+	while True:
+		input_ = Listen()
+		output_process = Think(input_)
+		Talk(output_process.output)
+		save_todisstree(output_process.tag,input_, output_process.output)
 
-while True:
-
-	input_ = Listen()
-	output_process = Think(input_)
-	Talk(output_process.output)
-	save_todisstree(output_process.tag,input_, output_process.output)
-
+if __name__ == "__main__":
+	wakeup()
